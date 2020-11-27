8bc5d49248c53fe5af394984c8e55c515b66cbca
MAKSYM <roman.sav15@gmail.com>
2020-11-27 12:23:17 +0200

    feates section, on click changes image (tabs)

--- #src/js/script.js
+++ #src/js/script.js
@@ -70,6 +70,24 @@ let front = {
             this.$body.removeClass('active')
         }
     },
+    openTab: function (element, tabName, parent) {
+      let i, tab_content, tab_links;
+
+      tab_content = $(element).closest(parent).find('.tab-content');
+
+      for (i = 0; i < tab_content.length; i++) {
+          tab_content[i].style.display = "none";
+      }
+
+      tab_links = $(element).closest('.tabs-ul').find('.tab-links');
+
+      for (i = 0; i < tab_links.length; i++) {
+          tab_links[i].className = tab_links[i].className.replace(" active", "");
+      }
+
+      document.getElementById(tabName).style.display = "block";
+      $(element).addClass('active');
+  },
 
   events: function () {
       let self = this;
